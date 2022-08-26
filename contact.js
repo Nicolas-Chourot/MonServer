450ffb70cc18d844702b79549e096574419a41db
Nicolas-Chourot <Nicolas.Chourot@clg.qc.ca>
2022-08-26 12:16:33 -0400

    first commit

---
+++ contact.js
@@ -0,0 +1,10 @@
+class Contact {
+    constructor(name, email, phone){
+        this.Id = 0;
+        this.Name = name;
+        this.Email = email;
+        this.Phone = phone
+    }
+}
+
+module.exports = Contact;
